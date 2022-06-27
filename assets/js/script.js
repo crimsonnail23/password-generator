30d8747c7081e64794e3a89b74bc4cfa3263788b
night <yseddiq@gmail.com>
2022-06-27 14:59:52 -0400

    added first function for character length

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,19 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var getPasswordLength = function(){
+  
+  window.prompt("How many characters do you want it to be? it has to be at least 8 and no more than 128 characters");
+
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
+
+  getPasswordLength();
+    
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
