0aa73f6aaf5f650ecae6de4653a94db0b56c99ff
night <yseddiq@gmail.com>
2022-06-27 19:56:47 -0400

    got the character type check to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,10 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
 
-  //to determine how long the password should be.
+  //following code to determine how long the password should be.
   var getLengthPrompt = window.prompt("How many characters do you want it to be? it has to be at least 8 and no more than 128 characters");
 
-  //change the data from window prompt from string to number.
+  //following code to change the data from window prompt from string to number.
   parseInt(getLengthPrompt,10);
   //create minimum and maximum requirement for length of password. 
   if(getLengthPrompt >=8 && getLengthPrompt <=128 ){
@@ -21,11 +21,11 @@ function writePassword() {
     window.alert("Invalid repsonse, try again");
 
 
-    //resets to beginning of function so the code doesn't keep going with an invalid response.
+    //following code resets to beginning of function so the code doesn't keep going with an invalid response.
     writePassword();
 
   }
-// to determine if there will be lowercase characters in the password.
+// following code to determine if there will be lowercase characters in the password.
   var lowercasePrompt = window.prompt("Do you want lowercase? type 1 for yes, 2 for no")
 
   if(lowercasePrompt == "1"){
@@ -35,19 +35,20 @@ function writePassword() {
 } else if(lowercasePrompt == "2") {
 
     window.alert("You do NOT want lower case");
+    console.log(lowercasePrompt);
 
 } else {
 
   window.alert("Invalid response, try again");
 
 
-  //resets to beginning of function so the code doesn't keep going with an invalid response.
+  //following code resets to beginning of function so the code doesn't keep going with an invalid response.
   writePassword();
 
 }
 
 
-//to determine if there will be uppercase characters in the password.
+//following code to determine if there will be uppercase characters in the password.
   var uppercasePrompt = window.prompt ("Do you want uppercase? type 1 for yes 2 for no.")
 
   if(uppercasePrompt == "1"){
@@ -64,12 +65,12 @@ function writePassword() {
   window.alert("invalid response, try again");
 
 
-  //resets to beginning of function so the code doesn't keep going with an invalid response.
+  //following code resets to beginning of function so the code doesn't keep going with an invalid response.
   writePassword();
 
 }
 
-  //to determine if there will be numbers in the password.
+  //following code to determine if there will be numbers in the password.
   var getNumbersPrompt = window.prompt("Do you want numbers in your password? types 1 for yes, and 2 for no");
 
   if (getNumbersPrompt == "1"){
@@ -84,7 +85,7 @@ function writePassword() {
 
   window.alert("Invalid response, try again");
 
-  //resets to beginning of function so the code doesn't keep going with an invalid response.
+  //following code resets to beginning of function so the code doesn't keep going with an invalid response.
   writePassword();
 
 }
@@ -105,15 +106,16 @@ function writePassword() {
   window.alert("Invalid Response");
 
 
-  //resets to beginning of function so the code doesn't continue with an invalid response.
+  //following code resets to beginning of function so the code doesn't continue with an invalid response.
   writePassword();
 
 }
 
-  //following code makes sure that at least one character type has been selected, 
-  if(lowercasePrompt == "2" && uppercasePrompt == "2" && getNumbersprompt == "2" && specialCharactersPrompt == "2"){
+  //following code makes sure that at least one character type has been selected,
+  
+  if(lowercasePrompt == "2" && uppercasePrompt == "2" && getNumbersPrompt == "2" && specialCharactersPrompt == "2"){
 
-  window.prompt("You need to select at least 1 character type.");
+  window.alert("You need to select at least 1 character type.");
 
   
   //if at least one character type isn't selected, then it'll reset the function so the code won't continue with a null set for all character types.
