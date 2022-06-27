b1bcf327bc150261fb65a01a89753e89aabdb83f
night <yseddiq@gmail.com>
2022-06-27 16:13:43 -0400

    fixed getLength function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,21 @@
 var generateBtn = document.querySelector("#generate");
 var getLength = function(){
   
-  window.prompt("How many characters do you want it to be? it has to be at least 8 and no more than 128 characters");
+  var getLengthPrompt = window.prompt("How many characters do you want it to be? it has to be at least 8 and no more than 128 characters");
+
+  parseInt(getLengthPrompt,10);
+  
+  if(getLengthPrompt >=8 && getLengthPrompt <=128 ){
+
+    window.alert("You have chosen " + getLengthPrompt);
+
+  } else{
+
+    window.alert("Invalid repsonse");
+
+    getLength();
+
+  }
 
 
 }
@@ -14,19 +28,20 @@ var getLowercase = function(){
 
   var lowercasePrompt = window.prompt("Do you want lowercase? type 1 for yes, 2 for no")
 
-    Number(lowercasePrompt)
 
-    if(lowercasePrompt===1){
+    if(lowercasePrompt === 1){
 
       window.alert("You have chosen lowercase");
 
-} else if(lowercasePrompt===2) {
+} else if(lowercasePrompt === 2) {
 
       window.alert("You do NOT want lower case")
 
 } else {
 
     window.alert("Invalid response")
+
+    getLowercase();
 
 }
 
