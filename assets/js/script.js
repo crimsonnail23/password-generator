de015ce8abd1d7be0c2ff425466a4bdf6ba19c5a
night <yseddiq@gmail.com>
2022-06-27 15:29:16 -0400

    added getUppercase function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,44 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var getPasswordLength = function(){
+var getLength = function(){
   
   window.prompt("How many characters do you want it to be? it has to be at least 8 and no more than 128 characters");
 
 
 }
 
+var getLowercase = function(){
+
+  var lowercasePrompt = window.prompt("Do you want lowercase? type 1 for yes, 2 for no")
+
+    Number(lowercasePrompt)
+
+    if(lowercasePrompt===1){
+
+      window.alert("You have chosen lowercase");
+
+} else if(lowercasePrompt===2) {
+
+      window.alert("You do NOT want lower case")
+
+} else {
+
+    window.alert("Invalid response")
+
+}
+
+
+}
+
+var getUppercase = function(){}
+
 
 // Write password to the #password input
 function writePassword() {
 
-  getPasswordLength();
+  getLength();
+  getLowercase();
     
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
