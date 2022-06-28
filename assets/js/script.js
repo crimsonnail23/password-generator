4e06454c45a7342c5810b568955f82318a2744c9
night <yseddiq@gmail.com>
2022-06-28 14:39:04 -0400

    finished special character array

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ var uppercaseLettersArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M"
 
 var numbersArray = ["0","1","2","3","4","5","6","7","8","9"];
 
-var specialCharactersArray = []
+var specialCharactersArray = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","~","`","[","]","{","}","|"]
 
 
 //following code will pull from the arrays depending on the user inputs from the writePassword function.
@@ -37,9 +37,9 @@ var specialCharactersArray = []
 function generatePassword(){
   if(passwordProperties.length>=8 && passwordProperties.length<=128 && passwordProperties.check==true){
 
-    for(var i=0; i<=passwordProperties.length; i++){
+    for(var i=0; i=passwordProperties.length; i++){
 
-      
+
     }
   }
 
